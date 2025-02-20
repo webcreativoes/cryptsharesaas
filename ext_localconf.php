fcf31cf753839a16f633d8b46a995feb8f4adbdd
webcreativoes <lutz.eckelmann@web-creativo.es>
2025-02-20 10:21:04 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,11 @@
 <?php
-//const KEY_TYPO3_CONF_VARS = 'TYPO3_CONF_VARS';
+defined('TYPO3') or die();
 
-defined('TYPO3_MODE') || die('Access denied.');
+// Logging für Deprecations aktivieren
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['deprecations']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::NOTICE] = [
+    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+        'logFile' => \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/deprecations.log'
+    ]
+];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:cryptsharesaas/Configuration/TSconfig/Page.typoscript">');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:cryptsharesaas/Configuration/TSconfig/User.typoscript">');
-
-$applicationContext = \TYPO3\CMS\Core\Core\Environment::getContext();
-if($applicationContext == 'Development' || $applicationContext == 'Development/Local') {
-    $GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['deprecations']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::NOTICE] = [];
-}
+// PageTSConfig muss in `Configuration/page.tsconfig` verschoben werden
