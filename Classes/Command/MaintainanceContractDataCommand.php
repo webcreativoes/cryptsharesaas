7406608287beb2347de7522ce72c57eec350e691
webcreativoes <lutz.eckelmann@web-creativo.es>
2025-02-26 11:05:56 +0100

    Update MaintainanceContractDataCommand.php

--- Classes/Command/MaintainanceContractDataCommand.php
+++ Classes/Command/MaintainanceContractDataCommand.php
@@ -1,73 +1,76 @@
 <?php
+declare(strict_types=1);
+
 namespace BefineSolutionsAG\Cryptsharesaas\Command;
 
+use DateTime;
+use DateInterval;
+use TYPO3\CMS\Core\Core\Environment;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 class MaintainanceContractDataCommand extends Command
 {
-    /**
-     * Configure the command by defining the name, options and arguments
-     */
-    protected function configure()
+    protected function configure(): void
     {
-        $this->setHelp('Prints a list of recent sys_log entries.' . LF . 'If you want to get more detailed information, use the --verbose option.');
+        $this->setName('cryptsharesaas:maintainance-contract-data')
+            ->setDescription('Deletes old contract data files and logs the process.')
+            ->setHelp('This command removes contract data files older than 2 days from the logging directory.');
     }
 
-    /**
-     * Executes the command for showing sys_log entries
-     *
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return int error code
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        switch (\TYPO3\CMS\Core\Core\Environment::getContext()) {
+        $envPath = match (true) {
+            Environment::getContext()->isProduction() => "/is/htdocs/wp13251579_RIVEMTPQCA/www/logging/",
+            Environment::getContext()->isDevelopment() => "/is/htdocs/wp13303627_IADXHPXADX/www/logging/",
+            Environment::getContext()->isTesting() => "",
+            default => "/is/htdocs/wp13303627_IADXHPXADX/www/logging/"
+        };
 
-            case 'Production':
-                $envPath = "/is/htdocs/wp13251579_RIVEMTPQCA/www/logging/";
-                break;
-            case 'Development/Local':
-                $envPath = "";
-                break;
-            case 'Development':
-            default:
-                $envPath = "/is/htdocs/wp13303627_IADXHPXADX/www/logging/";
-        }
-        $path = $envPath."contractData/";
+        $path = $envPath . "contractData" . DIRECTORY_SEPARATOR;
         $files = $this->getAllFilesFromFolder($path);
-        if ($files) {
-            $dt = new \DateTime('now');
+
+        if (!empty($files)) {
+            $dt = new DateTime('now');
             $date = $dt->format('Y-m-d');
-            $LogEntry = $date;
-            $dt->getTimestamp();
-            date_sub($dt, date_interval_create_from_date_string('2 days'));
+            $logEntry = $date;
+
+            // 2 Tage subtrahieren
+            $dt->sub(new DateInterval('P2D'));
             $oldTimeStamp = $dt->getTimestamp();
-            $fileNamePost = $envPath."Maintainance/contractData-".$date.".txt";
+
+            $fileNamePost = $envPath . "Maintainance" . DIRECTORY_SEPARATOR . "contractData-" . $date . ".txt";
             $maintenanceLog = fopen($fileNamePost, 'w');
+
+            if ($maintenanceLog === false) {
+                $output->writeln('<error>Failed to open log file: ' . $fileNamePost . '</error>');
+                return Command::FAILURE;
+            }
+
             foreach ($files as $value) {
-                $filepath = $path.$value;
-                $fileTime = date("F d Y H:i:s.", filectime($filepath));
-                if (filectime($filepath) <= $oldTimeStamp){
-                    $LogEntry .= filectime($filepath)."--".$fileTime.":"."-".$filepath."\r\n";
+                $filepath = $path . $value;
+                $fileTime = date("F d Y H:i:s.", @filectime($filepath));
+
+                if (@filectime($filepath) <= $oldTimeStamp) {
+                    $logEntry .= @filectime($filepath) . "--" . $fileTime . ":" . "-" . $filepath . PHP_EOL;
                     unlink($filepath);
                 }
             }
-            fwrite($maintenanceLog, $LogEntry);
+
+            fwrite($maintenanceLog, $logEntry);
             fclose($maintenanceLog);
         }
+
         return Command::SUCCESS;
     }
 
-    public function getAllFilesFromFolder($folder) {
+    private function getAllFilesFromFolder(string $folder): array
+    {
         $files = [];
-        if ( is_dir ( $folder ) && $handle = opendir($folder) ){
+        if (is_dir($folder) && ($handle = opendir($folder))) {
             while (($file = readdir($handle)) !== false) {
-                if($file != '.' && $file != '..'){
+                if ($file !== '.' && $file !== '..') {
                     $files[] = $file;
                 }
             }
